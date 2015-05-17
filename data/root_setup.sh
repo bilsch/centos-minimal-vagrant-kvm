77be976d3a0fc55e0f60b97aad8d4f0faa9c72b2
Bill Schwanitz <bilsch@bilsch.org>
2015-05-17 10:08:06 -0400

    root_setup tweaks

--- data/root_setup.sh
+++ data/root_setup.sh
@@ -3,8 +3,8 @@
 # Based on setup.sh from https://gun.io/blog/building-vagrant-machines-with-packer/
 
 yum -y upgrade
-groupadd -r admin
-usermod -a -G wheel vagrant
+yum -y install wget
+useradd -G admin vagrant
 cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
