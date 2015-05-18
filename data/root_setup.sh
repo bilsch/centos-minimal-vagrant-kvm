f553120af71b3610a75b5553bb5ed3479e164b13
Bill Schwanitz <bilsch@bilsch.org>
2015-05-17 22:52:04 -0400

    sync

--- data/root_setup.sh
+++ data/root_setup.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+set -x
+
 # Based on setup.sh from https://gun.io/blog/building-vagrant-machines-with-packer/
 
 yum -y upgrade
 yum -y install wget
-useradd -G admin vagrant
+/sbin/useradd -G wheel vagrant
 cp /etc/sudoers /etc/sudoers.orig
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
-sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=wheel' /etc/sudoers
+sed -i -e '/requiretty/s/^/#' -e '/NOPASSWD/s/^#//' /etc/sudoers
--- data/setup.sh
+++ data/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Installing vagrant keys
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-cd ~/.ssh
+cd ~vagrant
+mkdir .ssh
+chmod 700 .ssh
+cd .ssh
 wget --no-check-certificate 'https://raw.github.com/bilsch/centos-minimal-vagrant-kvm/master/data/insecure-vagrant.pub' -O authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-chown -R vagrant ~/.ssh
+chmod 600 authorized_keys
+chown -R vagrant ~vagrant
