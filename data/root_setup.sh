adc93db938e4eac3cc49e17000f49f68e7777dc6
Bill Schwanitz <bilsch@bilsch.org>
2015-05-18 16:08:57 -0400

    tweaks

--- data/root_setup.sh
+++ data/root_setup.sh
@@ -8,5 +8,6 @@ yum -y upgrade
 yum -y install wget
 /sbin/useradd -G wheel vagrant
 cp /etc/sudoers /etc/sudoers.orig
-sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=wheel' /etc/sudoers
-sed -i -e '/requiretty/s/^/#' -e '/NOPASSWD/s/^#//' /etc/sudoers
+mv /tmp/sudoers /etc
+chmod 0440 /etc/sudoers
+chown root:root /etc/sudoers
